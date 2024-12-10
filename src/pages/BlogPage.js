86452905b06c0f7d69404cedcd7f82e4f8be3577
Nostromo714 <93149393+Nostromo714@users.noreply.github.com>
2024-12-10 13:53:23 -0500

    took out h1 not being used. updated fex to grid layout with 3 rows for blogs and stack resopsive when screen is smaller

--- src/pages/BlogPage.js
+++ src/pages/BlogPage.js
@@ -79,13 +79,11 @@ const BlogPage = () => {
             <CommentSection blogId={selectedBlog._id} />
           </div>
         ) : (
-          <div className="flex flex-wrap justify-center gap-8">
-           <h1 className="text-3xl font-bold text-center mb-5"></h1>
-
+          <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8">
             {blogs.map((blog) => (
               <div
                 key={blog._id}
-                className="bg-white p-8 rounded-xl shadow-lg w-full sm:w-1/2 md:w-1/2 lg:w-1/3 max-w-sm flex flex-col justify-between h-full relative border border-gray-200 transition-all duration-300 ease-in-out transform hover:scale-105"
+                className="bg-white p-8 rounded-xl shadow-lg w-full flex flex-col justify-between h-full relative border border-gray-200 transition-all duration-300 ease-in-out transform hover:scale-105"
               >
                 <h2 className="text-2xl font-semibold text-gray-800 mb-4">{blog.title}</h2>
                 <p className="text-sm text-gray-500 mb-3">
