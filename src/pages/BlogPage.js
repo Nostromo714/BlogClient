a58e118e37edc54868bdd70e477176935cfbd336
Nostromo714 <93149393+Nostromo714@users.noreply.github.com>
2024-12-18 11:21:05 -0500

    updated h1 to text-5xl

--- src/pages/BlogPage.js
+++ src/pages/BlogPage.js
@@ -51,7 +51,7 @@ const BlogPage = () => {
   return (
 
     <div className="min-h-screen bg-gradient-to-r from-orange-100 via-orange-200 to-orange-100 flex flex-col justify-center items-center pt-12 pb-12">
-      <h1 className="text-3xl font-serif font-bold text-center mb-5">Welcome to the Blog Page</h1>
+      <h1 className="text-5xl pb-20 font-serif font-bold text-center mb-5">Welcome to the Blog Page</h1>
       <div className="w-full max-w-6xl px-4">
         {error && <p className="text-red-500 text-center">{error}</p>}
 
